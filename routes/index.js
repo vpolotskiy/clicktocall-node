c6b10f45b3df30f8085b7e29c334027a2e9ecddf
Vitaliy Polotskiy <vpolotskiy@gmail.com>
2017-05-17 00:05:58 +0300

    Fixes

--- routes/index.js
+++ routes/index.js
@@ -68,7 +68,9 @@ module.exports = function(app) {
         var salesNumber = request.params.salesNumber;
         var twimlResponse = new VoiceResponse();
 
-        twimlResponse.say('Thanks for contacting our sales department. Our next available representative will take your call. Thanks for contacting our sales department. Our next available representative will take your call.',
+        twimlResponse.say('Thanks for contacting our sales department. Our ' +
+                          'next available representative will take your call. ' +
+                                            'Lorem ipsum si dolor amet. Lorem ipsum si dolor amet.',
                           { voice: 'alice' });
 
         twimlResponse.dial(salesNumber);
